6d2b09f1449b693435650d7da49c5bb50f6cdc0b
ashar73 <ashar.alam786@gmail.com>
2021-05-12 14:41:22 +0530

    adding go

---
+++ conditional.go
@@ -0,0 +1,23 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+	//x:=5
+	//y:=5.5
+	//val:= x<5 // here its checking if x is less than 5 and returns FAlse to val
+	//val := x<=5 // here its checking if x is less than equalto 5 and returns true to val
+	//val := y>float64(x) //had to convert xinto float coz datatype should be same
+	//...................................................................
+	// x:="ashar"
+	// y:="Ashar"
+	// val:= x==y
+	// fmt.Printf("%t", val)
+// OP: false {coz A in y and a in x are not same}
+	// x:="a"//ASCI 97
+	// y:="A"//ASCI 65
+	// val:= x>y
+	// fmt.Printf("%t",val)
+//OP: true {coz it compares the ASCI value of A and a}
+
+}
